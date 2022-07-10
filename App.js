f68ebf6e6537f4d1e85cfd48045f9af6949f48c2
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-07-11 01:54:45 +0900

    init

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+const { height: HEIGHT } = Dimensions.get("window");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.purple}>
+        <Text style={styles.hello}>Hello</Text>
+      </View>
+      <ScrollView style={styles.teal}>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+        <Text style={styles.scroll}>So Cool</Text>
+        <Text>So sexy!!!</Text>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  purple: {
+    flex: 1,
+    backgroundColor: "purple",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  hello: {
+    fontSize: 50,
+    height: 100,
+    color: "white",
+    fontWeight: "400",
+  },
+  teal: {
+    backgroundColor: "teal",
+    height: "50%",
+  },
+  scroll: {
+    fontSize: 100,
+    fontWeight: "600",
   },
 });
